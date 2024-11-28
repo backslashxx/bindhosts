0736374f3bc3299f57b93608bbca37ca56799cfb
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-11-28 11:54:45 +0800

    scripts/uninstall: cleanup for helper modes

--- uninstall.sh
+++ uninstall.sh
@@ -1,1 +1,7 @@
 rm -rf /data/adb/bindhosts
+# cleanup for helper modes
+# hfr
+[ -f /data/adb/hosts ] && printf "127.0.0.1 localhost\n::1 localhost\n" > /data/adb/hosts
+# znhr
+[ -f /data/adb/hostsredirect/hosts ] && printf "127.0.0.1 localhost\n::1 localhost\n" > /data/adb/hostsredirect/hosts
+
