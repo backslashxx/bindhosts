608512053f3b541f9ff9a0921b6908e86a2acefe
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-03 22:40:42 +0800

    scripts/customize: drop old version migrations
    at this point most users shouldve been migrated

--- module/customize.sh
+++ module/customize.sh
@@ -10,9 +10,7 @@ echo "[+] bindhosts v$versionCode "
 echo "[%] customize.sh "
 
 # persistence
-if [ ! -d $PERSISTENT_DIR ] ; then
-	mkdir -p $PERSISTENT_DIR
-fi
+[ ! -d $PERSISTENT_DIR ] && mkdir -p $PERSISTENT_DIR
 
 # check for other systemless hosts modules and disable them
 
@@ -38,22 +36,6 @@ if [ -f /data/adb/modules/systemless-hosts-KernelSU-module/system/etc/hosts ] ; 
 	echo "[+] migrating hosts file "
 	cat /data/adb/modules/systemless-hosts-KernelSU-module/system/etc/hosts > $MODPATH/system/etc/hosts
 fi
-
-# bindhosts-master =< 145
-if [ -f /data/adb/modules/bindhosts/hosts ] ; then
-	echo "[+] migrating hosts file "
-	cat /data/adb/modules/bindhosts/hosts > $MODPATH/system/etc/hosts
-fi
-
-# handle upgrades/reinstalls
-# pre persist migration
-files="system/etc/hosts blacklist.txt custom.txt sources.txt whitelist.txt"
-for i in $files ; do
-	if [ -f /data/adb/modules/bindhosts/$i ] ; then
-		echo "[+] migrating $i "
-		cat /data/adb/modules/bindhosts/$i > $MODPATH/$i
-	fi	
-done
 
 # normal flow for persistence
 # move over our files, remove after
