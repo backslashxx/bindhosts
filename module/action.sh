b92dc6898778bb5f2661b1c2fcef3a693ee1b31c
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-11 18:57:51 +0800

    scripts/action: handle magisk_webui_redirect logical case
    This change fully turns action.sh into a wrapper for bindhosts.sh.
    
    If magisk_webui_redirect = 1, check for mmrl and ksuwebui to open WebUI,
    fallback to action if both fail.
    --
    magisk_webui_redirect = 1 (default)
    
    if magisktmp && magisk_webui_redirect
    	if mmrl, open webui, exit
    	if ksuwebui, open webui, exit
    	if both fail, run action, exit
    else
    	[x] will not enter condition
    fi
    --
    
    If magisk_webui_redirect = 0, skip WebUI logic and run action.
    
    magisk_webui_redirect = 0 (override)
    
    if magisktmp && magisk_webui_redirect
    	[x] will not enter condition
    else
    	run action, exit
    fi
    --
    
    Non-Magisk (Apatch/KernelSU) setup skips WebUI redirection logic and runs action.
    
    test logic for non-magisk (apatch / ksu)
    
    if magisktmp && magisk_webui_redirect
    	[x] will not enter condition
    else
    	run action, exit
    fi
    
    Co-Authored-By: KOWX712 <185127155+KOWX712@users.noreply.github.com>

--- module/action.sh
+++ module/action.sh
@@ -4,8 +4,10 @@ MODDIR="/data/adb/modules/bindhosts"
 PERSISTENT_DIR="/data/adb/bindhosts"
 . $MODDIR/mode.sh
 
+magisk_webui_redirect=1
 
-
+# action.sh
+# this is now a shim for bindhosts.sh
 force_update() {
 	sh $MODDIR/bindhosts.sh --force-update
 }
@@ -30,7 +32,27 @@ case "$1" in
 	--toggle-updatejson) toggle_updatejson; exit ;;
 esac
 
-# detect magisk here
-# need logic
+# read webui setting here
+# echo "magisk_webui_redirect=0" > /data/adb/bindhosts/webui_setting.sh
+[ -f $PERSISTENT_DIR/webui_setting.sh ] && . $PERSISTENT_DIR/webui_setting.sh
 
-sh $MODDIR/bindhosts.sh
+# detect magisk environment here
+if [ ! -z "$MAGISKTMP" ] && [ $magisk_webui_redirect = 1 ] ; then
+	# courtesy of kow
+	if pm list packages | grep -q "com.dergoogler.mmrl"; then
+		echo "- Launching WebUI in MMRL WebUI..."
+		am start -n "com.dergoogler.mmrl/.ui.activity.webui.WebUIActivity" -e MOD_ID "bindhosts"
+		exit 0
+	fi
+	if pm list packages | grep -q "io.github.a13e300.ksuwebui"; then
+		echo "- Launching WebUI in KSUWebUIStandalone..."
+		am start -n "io.github.a13e300.ksuwebui/.WebUIActivity" -e id "bindhosts"
+		exit 0
+	fi
+	sh $MODDIR/bindhosts.sh
+	exit 0
+else
+	sh $MODDIR/bindhosts.sh
+	exit 0
+fi
+
