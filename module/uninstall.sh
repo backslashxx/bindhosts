04af06a89fc5d7e6f86e85f6024ebb5d5b190c10
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-16 00:55:44 +0800

    scripts/uninstall: remove symlink at uninstall

--- module/uninstall.sh
+++ module/uninstall.sh
@@ -1,4 +1,9 @@
+#!/bin/sh
+# delete settings
 rm -rf /data/adb/bindhosts
+# delete symlink
+manager_paths="/data/adb/ap/bin /data/adb/ksu/bin"
+for i in $manager_paths; do rm -f $i/bindhosts ; done
 # cleanup for helper modes
 # hfr
 [ -f /data/adb/hosts ] && printf "127.0.0.1 localhost\n::1 localhost\n" > /data/adb/hosts
