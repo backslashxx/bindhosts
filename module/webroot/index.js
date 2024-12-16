3ee33920747dbd2ee5ef894cd2bd71aaee2ecdb7
KOWX712 <leecc0503@gmail.com>
2024-12-17 01:43:47 +0800

    webui: minor adjust ui
    adjust position of control panel icon. remove 'status:' in Status Panel.

--- module/webroot/index.js
+++ module/webroot/index.js
@@ -143,7 +143,7 @@ async function checkCronStatus() {
 // Function to get the status from module.prop and update the status in the WebUI
 async function updateStatusFromModuleProp() {
     try {
-        const command = "grep '^description=' /data/adb/modules/bindhosts/module.prop | cut -d'=' -f2";
+        const command = "grep '^description=' /data/adb/modules/bindhosts/module.prop | cut -d'=' -f2 | sed 's/status: //'";
         const description = await execCommand(command);
         updateStatus(description.trim());
     } catch (error) {
